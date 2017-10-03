27228fb4970ddcbe1a26551966ca34f60ad05f16
zoe <z.zoelec2@gmail.com>
2017-10-03 16:20:56 -0700

    Add OpticalDistanceSensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/OpticalDistanceSensor.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "Sensor: MR ODS", group = "Sensor")
+@Disabled
+
+public class OpticalDistanceSensor extends LinearOpMode {
+    com.qualcomm.robotcore.hardware.OpticalDistanceSensor odsSensor;  // Hardware Device Object
+
+    @Override
+    public void runOpMode() {
+
+        // get a reference to our Light Sensor object.
+        odsSensor = hardwareMap.get(com.qualcomm.robotcore.hardware.OpticalDistanceSensor.class, "sensor_ods");
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // while the op mode is active, loop and read the light levels.
+        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+        while (opModeIsActive()) {
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Raw",    odsSensor.getRawLightDetected());
+            telemetry.addData("Normal", odsSensor.getLightDetected());
+
+            telemetry.update();
+        }
+    }
+}
